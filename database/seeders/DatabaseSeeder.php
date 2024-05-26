10f929b998add2bd2aaf06aa660f29fbc3de8fe5
Vittorio Grimaldi <grimaldi.vittorio01@gmail.com>
2024-05-26 10:30:32 +0200

    added to db seeder airport and database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             AirlineSeeder::class,
+            AirportSeeder::class,
+            FlightSeeder::class,
         ]);
     }
 }
